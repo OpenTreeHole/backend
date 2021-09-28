cdbf2ef0d8825b035aee719d5ba1fb2da4ac4edf
hasbai <jsclndnz@outlook.com>
2021-09-28 18:27:25 +0800

    uvicorn + gunicorn 部署

---
+++ gconfig.py
@@ -0,0 +1,36 @@
+# gunicorn 配置文件
+import multiprocessing
+
+# 协程需要此补丁
+# form gevent import monkey
+# monkey.patch_all()
+# 其他非uvicorn需打开此配置
+# worker_class = "gevent"
+
+# ip + port
+bind = "127.0.0.1:80"
+# 超时时间
+timeout = 40
+
+# 并行工作进程数
+workers = multiprocessing.cpu_count() * 2 + 1
+# 每个进程开启的线程数
+threads = 2
+# 服务器中在pending状态的最大连接数 (建议 64-2048)
+backlog = 2048
+
+worker_class = "uvicorn.workers.UvicornWorker"
+
+# 客户端同时最大连接数，适用于 gevent eventlet
+worker_connections = 1000
+
+# 以守护进程形式运行：后台运行
+daemon = False
+
+loglevel = 'debug'
+pidfile = 'log/gunicorn.pid'
+accesslog = 'log/gun-access.log'
+errorlog = 'log/gun-error.log'
+
+# reload=true 自动重启
+# chdir = '/path/' 指定它的工作路径
