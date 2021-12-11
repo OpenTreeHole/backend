fc4e865496d2c03c415557ea8ca50629396bc150
hasbai <jsclndnz@outlook.com>
2021-12-12 00:08:44 +0800

    #55

--- api/serializers.py
+++ api/serializers.py
@@ -176,8 +176,8 @@ class FloorSerializer(SimpleFloorSerializer):
 
     class Meta:
         model = Floor
-        fields = ['floor_id', 'hole_id', 'content', 'anonyname', 'mention', 'time_updated', 'time_created', 'deleted', 'fold', 'like']
-        read_only_fields = ['floor_id', 'anonyname']
+        fields = ['floor_id', 'hole_id', 'content', 'history', 'anonyname', 'mention', 'time_updated', 'time_created', 'deleted', 'fold', 'like']
+        read_only_fields = ['floor_id', 'history', 'anonyname']
 
     @staticmethod
     def get_queryset(queryset):
