35e2d82ee0505178ec6eda855d0f8a4e082180eb
顶碗人 <jsclndnz@outlook.com>
2022-04-18 21:40:26 +0800

    fix auth error

--- utils/my_auth.py
+++ utils/my_auth.py
@@ -12,43 +12,28 @@ from api.models import User
 
 
 class MyTokenAuthentication(TokenAuthentication):
-    def _authenticate(self, auth_method, token):
-        if auth_method == 'token':
-            return self.authenticate_credentials(token)
-        elif auth_method == 'bearer':
-            try:
-                payload = jwt.decode(token, verify=False, options={"verify_signature": False})
-            except jwt.DecodeError:
-                raise AuthenticationFailed('jwt token invalid')
-            _id = payload.get('uid')
-            try:
-                user = User.objects.get(id=_id)
-            except User.DoesNotExist:
-                email = f'user#{_id}@fduhole.com'
-                user = User.objects.create_user(id=_id, email=email, password=email)
-            return user, token
-
     def authenticate(self, request):
         if request.headers.get('x-anonymous-consumer'):
             return
         auth = get_authorization_header(request).split()
         if len(auth) != 2:
             return
-        try:
-            uid = int(request.headers.get('x-consumer-username'))
-        except:
-            return
-        try:
-            user = User.objects.get(id=uid)
-        except User.DoesNotExist:
-            email = f'user#{uid}@fduhole.com'
-            user = User.objects.create_user(id=uid, email=email, password=email)
+        token = auth[1].decode()
+        uid = request.headers.get('x-consumer-username')
+        if not uid:
+            user, token = self.authenticate_credentials(token)
+        else:
+            try:
+                user = User.objects.get(id=uid)
+            except User.DoesNotExist:
+                email = f'user#{uid}@fduhole.com'
+                user = User.objects.create_user(id=uid, email=email, password=email)
         cache.set(
             f'user_last_login_{user.id}',
             datetime.now(settings.TIMEZONE).isoformat(),
             86400
         )
-        return user, auth[1].decode()
+        return user, token
 
 
 async def async_token_auth(request):
