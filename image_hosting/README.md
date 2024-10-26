468e6bd1c4409911f82523a282ca6bf7f5edcc25
SeleiXi <bzhanthresh@gmail.com>
2024-10-26 18:56:01 +0800

    refactor: remove irrelevant code

--- image_hosting/README.md
+++ image_hosting/README.md
@@ -1,12 +1,1 @@
-# treehole image proxy
-
-a image proxy from chevereto api to lsky api
-
-## usage
-
-```shell
-# build for release
-go build -tags "release" -ldflags "-s -w" -o treehole.exe
-# run
-./treehole.exe
-```
+# treehole image hosting
--- image_hosting/config.go
+++ image_hosting/config.go
@@ -1,49 +1,51 @@
 package main
 
-import (
-	"net/url"
-	"sync"
-)
-
-var Config struct {
-	ApiStyle      string `env:"API_STYLE" envDefault:"chevereto"`
-	ProxyType     string `env:"PROXY_TYPE" envDefault:"lskypro"`
-	ProxyUrl      string `env:"PROXY_URL,required"`
-	ProxyUsername string `env:"PROXY_USERNAME"`
-	ProxyPassword string `env:"PROXY_PASSWORD,required"`
-	ProxyEmail    string `env:"PROXY_EMAIL"`
-	HostRewrite   string `env:"HOST_REWRITE"`
-	Token         string `env:"TOKEN"`
-}
-
-var Token struct {
-	sync.RWMutex
-	data string
-}
-
-var ProxyUrlData *url.URL
-
-func GetToken() string {
-	Token.RLock()
-	defer Token.RUnlock()
-	return Token.data
-}
-
-// func init() {
-// 	err := env.Parse(&Config)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	ProxyUrlData, err = url.ParseRequestURI(Config.ProxyUrl)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	if Config.Token == "" {
-// 		Config.Token, err = LskyRefreshToken()
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 	}
-// 	Token.data = Config.Token
-// 	log.Printf("%+v", Token.data)
-// }
+//
+//import (
+//	"github.com/caarlos0/env/v9"
+//	"net/url"
+//	"sync"
+//)
+//
+//var Config struct {
+//	ApiStyle      string `env:"API_STYLE" envDefault:"chevereto"`
+//	ProxyType     string `env:"PROXY_TYPE" envDefault:"lskypro"`
+//	ProxyUrl      string `env:"PROXY_URL,required"`
+//	ProxyUsername string `env:"PROXY_USERNAME"`
+//	ProxyPassword string `env:"PROXY_PASSWORD,required"`
+//	ProxyEmail    string `env:"PROXY_EMAIL"`
+//	HostRewrite   string `env:"HOST_REWRITE"`
+//	Token         string `env:"TOKEN"`
+//}
+//
+//var Token struct {
+//	sync.RWMutex
+//	data string
+//}
+//
+//var ProxyUrlData *url.URL
+//
+//func GetToken() string {
+//	Token.RLock()
+//	defer Token.RUnlock()
+//	return Token.data
+//}
+//
+//func init() {
+//	err := env.Parse(&Config)
+//	if err != nil {
+//		panic(err)
+//	}
+//	ProxyUrlData, err = url.ParseRequestURI(Config.ProxyUrl)
+//	if err != nil {
+//		panic(err)
+//	}
+//	if Config.Token == "" {
+//		Config.Token, err = LskyRefreshToken()
+//		if err != nil {
+//			panic(err)
+//		}
+//	}
+//	Token.data = Config.Token
+//	log.Printf("%+v", Token.data)
+//}
