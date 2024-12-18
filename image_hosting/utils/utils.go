b6cef4c30eb9595e519ea65e3fbbd96d8a4c4b46
SeleiXi <bzhanthresh@gmail.com>
2024-12-18 23:46:44 +0800

    refactor: generate identifier in 14 characters

--- image_hosting/utils/utils.go
+++ image_hosting/utils/utils.go
@@ -9,16 +9,16 @@ import (
 
 func GenerateIdentifier() (string, error) {
 	now := time.Now().UnixMicro()
-
-	// Generate a random 6-byte (12-character) string
-	randomBytes := make([]byte, 6)
+	nowStr := fmt.Sprintf("%x", now)[:8] // first 8 characters of the timestamp
+	// Generate a random 6 character string, 14 characters in total (different from the original image proxy (13 characters) )
+	randomBytes := make([]byte, 3)
 	if _, err := rand.Read(randomBytes); err != nil {
 		return "", err
 	}
 	randomSuffix := hex.EncodeToString(randomBytes)
 
 	// Combine the timestamp and random suffix
-	return fmt.Sprintf("%x%s", now, randomSuffix), nil
+	return fmt.Sprintf("%s%s", nowStr, randomSuffix), nil
 }
 
 func IsAllowedExtension(ext string) bool {
